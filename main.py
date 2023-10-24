fc6502e1fd5ae25c8557f4bfa6e07b4fb7e85325
WhatTheBoy <boychenko.vladislavv@gmail.com>
2023-10-24 13:17:57 +0300

    string

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-my_dict = {}
-
-first_key = input('Введите ключ 1:')
-first_mean = input('Введите значение 1 к ключу 1:')
-my_dict[first_key] = first_mean
-
-secound_key = input('Введите ключ 2:')
-secound_mean = input('Введите значение 2 к ключу 2:')
-my_dict[secound_key] = secound_mean
-
-third_key = input('Введите ключ 3:')
-third_mean = input('Введите значение 3 к ключу 3:')
-my_dict[third_key] = third_mean
-
-
-my_dict['brand'] = 'bmw'
-my_dict['color'] = 'green'
-print(len(my_dict))
-print(my_dict['brand'])
-print(my_dict.get('color'))
-del my_dict['size']
-new_dict = my_dict.copy()
-new_dict['where'] = 'UK'
-print(new_dict.items())
-print(list(new_dict.popitem()))
-print(new_dict.keys())
-print(new_dict)
-print(new_dict.clear())
-
-print(my_dict)
-print(new_dict)
+print('''
+ {0:15.4f} {1:15.4f} {2:15.4f} {3:15.4f}
+ {4:15.4f} {5:15.4f} {6:15.4f} {7:15.4f}
+ {8:15.4f} {9:15.4f} {10:15.4f} {11:15.4f}
+      '''.format(
+    1.233, 123.234, 321.2323243, 6.3213232,
+    2.34323, 5.2343234, 4.3322332, 5.32132312,
+    4.12313232, 923.32342324, 2323.423342324, 4.432344
+))
